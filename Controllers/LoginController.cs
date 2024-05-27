e73cd51af62f7a88fffd1cc4d31c931236593346
Gustavo Gomes <gustavo020sp@gmail.com>
2024-05-27 09:27:25 -0300

    Created Login view and controller

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+	public class LoginController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -49,7 +49,7 @@ namespace SalesWebMvc
 
 			app.MapControllerRoute(
 				name: "default",
-				pattern: "{controller=Home}/{action=Index}/{id?}");
+				pattern: "{controller=Login}/{action=Index}/{id?}");
 
 			app.Run();
 		}
